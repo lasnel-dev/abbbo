2b3e1a8f61302239bbb936052efd2f981f520af8
lasnel-dev <novak_i@itstep.org>
2024-12-08 18:04:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,29 +15,35 @@ cap_width = 500
 
 while True:
     ret, img = cap.read()
-    face = face_cascade.detectMultiScale(img, 1.3)
+    face = face_cascade.detectMultiScale(img, 1.1)
 
     for (x, y, w, h) in face:
         cv2.rectangle(img,(x,y), (x+w,y+h), (0, 255, 0), 3)
 
-        #
-        # arr = {y: y + h, x: x + w}
-        # print("----------------")
-        # print("----------------")
-        # print(arr)
-        # print('X: %d      | Y:%d' % (x,y))
-        # print('x+w %d       | y+h^ %d' % (x+w, y +h))
-        #
-        # xx = int(x + (x + h))/2
-        # yy = int(y + (y + w))/2
-        # center = (xx, yy)
-        # print("Center of Rectangle: ", center)
-        #
-        # data = "X{0:d}Y{1:d}Z".format(xx, yy)
-        # arduino.write(data)
+
+        arr = {y: y + h, x: x + w}
+        print("----------------")
+        print("----------------")
+        print(arr)
+        print('X: %d      | Y:%d' % (x,y))
+        print('x+w %d       | y+h^ %d' % (x+w, y +h))
+
+        xx = int(x + (x + h))/2
+        yy = int(y + (y + w))/2
+        center = (int(xx), int(yy))
+        print("Center of Rectangle: ", center)
+
+        data = "X{0}Y{1}Z".format(int(xx), int(yy))
+        print("output = '" + data + "'" )
+        a = str(xx)
+
+        arduino.write(str.encode(a))
 
     cv2.imshow('Stream', img)
     key = cv2.waitKey(20)
     if key == 27:
         print("Stop Streaming")
         break
+
+cap.release()
+cv2.destroyAllWindows()
